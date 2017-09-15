9c4708171eb50a8438998897ccd137f14d662279
kameron19921016 <kameron19921016@yahoo.com>
2017-09-15 09:13:30 +0800

    FWB: Add Volume Key Answer (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4825,6 +4825,12 @@ public final class Settings {
         public static final Validator SHOW_FOURG_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * volume answer
+         * @hide
+         */
+        public static final String ANSWER_VOLUME_BUTTON_BEHAVIOR_ANSWER = "call_volume_answer";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
